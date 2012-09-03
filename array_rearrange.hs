6a9b8e97275e4be8570d0da92b9caec9e9969dcb
dfordivam <dfordivam@gmail.com>
2012-09-03 16:41:56 +0530

    updating

--- array_rearrange.hs
+++ array_rearrange.hs
@@ -1,10 +1,23 @@
 getNewIndex :: Int -> Int -> Maybe Int
 
 getNewIndex     n i 
-            | i < 1 || i > 3n = Nothing
-            | i < (n + 1)   = 3(i-1) + 1 -- a
-            | i < (2n + 1)  = 3(i-1) + 2 -- b
-            | otherwise     = 3(i-1) + 3 -- c
+            | i < 1 || i > 3*n || n < 2 = Nothing
+            | i < (n + 1)   = Just (3*(i-1) + 1) -- a
+            | i < (2*n + 1) = Just (3*( (i - n ) -1) + 2) -- b
+            | otherwise     = Just (3*( (i - 2*n ) -1) + 3) -- c
 
+getNewIndex1 :: Int -> Int -> Int
 
+getNewIndex1     n i 
+            | i < 1 || i > 3*n || n < 2 = 0
+            | i < (n + 1)   =  (3*(i-1) + 1) -- a
+            | i < (2*n + 1) =  (3*( (i - n ) -1) + 2) -- b
+            | otherwise     =  (3*( (i - 2*n ) -1) + 3) -- c
 
+getPath :: [Int] -> Int -> [Int]
+
+getPath list i 
+            | i == length list  = []
+            | i == 0            = getPath list 2
+            | otherwise         = val : getPath list val
+        where val = list !! (i - 1 )
