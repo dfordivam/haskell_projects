7193545b6d89bd3041d980e35ed43ca54f98dd12
dfordivam <dfordivam@gmail.com>
2013-06-05 19:06:42 +0530

    get list of insts working

--- verilog_related/check_mod_use.hs
+++ verilog_related/check_mod_use.hs
@@ -8,6 +8,7 @@ import Text.Parsec.ByteString       (parseFromFile)
 import Language.Verilog.Parser
 import Language.Verilog.PrettyPrint (ppVerilog)
 import Language.Verilog.Syntax      
+import Language.Verilog.Syntax.AST      
 
 --------------------------------------------------------------------------------
 
@@ -25,7 +26,7 @@ run fp
 
 checkRun fp = do 
             v <- run fp
-            let retVal = checkModUse v
+            let retVal = checkModInst v
             return retVal
 
 checkModUse :: Verilog -> [Ident]
@@ -33,8 +34,27 @@ checkModUse :: Verilog -> [Ident]
 checkModUse (Verilog v) = unused_mod_list
    where unused_mod_list = map getDescName v
 
-getDescName (ModuleDescription mod) = name
-            where name = (\(Module name _ _) -> name) mod
-getDescName (UDPDescription udp) = name
-            where name = (\(UDP name _ _ _ _ _)-> name) udp
+getDescName (ModuleDescription mod) = modName mod
+getDescName (UDPDescription udp) = udpName udp
+
+listInstatiatedModules :: Module -> Maybe [Ident]
+listInstatiatedModules mod = return (insts)
+    where body = (\(Module _ _ b) -> b) mod
+          insts = map getInstanceName body
+
+getInstanceName :: Item -> Ident
+getInstanceName (InstanceItem i) = (\(Instance x _ _) -> x ) i
+getInstanceName _ = Ident "dummy"
+
+getModule :: Description -> Maybe Module
+getModule (ModuleDescription mod) = Just mod
+getModule (UDPDescription _) = Nothing
+
+--checkModInst :: Verilog -> [Maybe Module]
+checkModInst (Verilog v) = (map checkModInstReq v)
+checkModInstReq ds = (getModule ds) >>? listInstatiatedModules
+
+(>>?) :: Maybe a -> (a -> Maybe b) -> Maybe b
+Nothing >>? _ = Nothing
+Just v >>? f = f v 
 
