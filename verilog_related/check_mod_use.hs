fede95989c3c7a3d1438993c33449f78e5d0b76d
dfordivam <dfordivam@gmail.com>
2013-06-11 17:52:35 +0530

    Improved the checkModInst to return unique instance usage names

--- verilog_related/check_mod_use.hs
+++ verilog_related/check_mod_use.hs
@@ -6,10 +6,12 @@ import Text.Parsec                  (parse)
 import Text.Parsec.ByteString       (parseFromFile)
 
 import Language.Verilog.Parser
-import Language.Verilog.PrettyPrint (ppVerilog)
+import Language.Verilog.PrettyPrint 
 import Language.Verilog.Syntax      
 import Language.Verilog.Syntax.AST      
-
+import Control.Monad
+import Data.Maybe
+import Data.List
 --------------------------------------------------------------------------------
 
 test :: FilePath -> IO ()
@@ -27,6 +29,7 @@ run fp
 checkRun fp = do 
             v <- run fp
             let retVal = checkModInst v
+            print retVal
             return retVal
 
 checkModUse :: Verilog -> [Ident]
@@ -37,24 +40,18 @@ checkModUse (Verilog v) = unused_mod_list
 getDescName (ModuleDescription mod) = modName mod
 getDescName (UDPDescription udp) = udpName udp
 
-listInstatiatedModules :: Module -> Maybe [Ident]
 listInstatiatedModules mod = return (insts)
     where body = (\(Module _ _ b) -> b) mod
-          insts = map getInstanceName body
+          insts = map getInstanceItemName body
 
-getInstanceName :: Item -> Ident
-getInstanceName (InstanceItem i) = (\(Instance x _ _) -> x ) i
-getInstanceName _ = Ident "dummy"
+getInstanceItemName (InstanceItem i) = Just ((\(Instance x _ _) -> x ) i)
+getInstanceItemName _ = Nothing
 
 getModule :: Description -> Maybe Module
 getModule (ModuleDescription mod) = Just mod
 getModule (UDPDescription _) = Nothing
 
---checkModInst :: Verilog -> [Maybe Module]
-checkModInst (Verilog v) = (map checkModInstReq v)
-checkModInstReq ds = (getModule ds) >>? listInstatiatedModules
-
-(>>?) :: Maybe a -> (a -> Maybe b) -> Maybe b
-Nothing >>? _ = Nothing
-Just v >>? f = f v 
+-- Returns list of Modules being instantiated in Verilog v
+checkModInst (Verilog v) = nub (map fromJust (concat (catMaybes (map checkModInstReq v))))
+checkModInstReq ds = (getModule ds) >>= listInstatiatedModules
 
